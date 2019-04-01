c651dfc2808597ee7caebb4e34cac78c6917cd78
David Alves <david@cortexxus.com>
2019-04-01 13:21:01 -0500

    APSP runs faster than dijkstra
    Use a on-stack bitvector for 'fixed' to eliminate the memory
    bottleneck that was causing us to be slower than dijkstra.
    
    This is a messy commit that likely needs to be cleaned up.

---
+++ lonestar/sssp/bf/all.hpp
@@ -0,0 +1,10 @@
+#ifndef BF_ALL_HPP
+#define BF_ALL_HPP
+
+//#include "bloom_filter/a2.hpp"
+#include "bloom_filter/basic.hpp"
+//#include "bloom_filter/bitwise.hpp"
+//#include "bloom_filter/counting.hpp"
+//#include "bloom_filter/stable.hpp"
+
+#endif
